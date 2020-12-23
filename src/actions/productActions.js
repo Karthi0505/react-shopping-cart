8f30a88c48eeafe4effdafeaa0dea7d61984f123
unknown <karthi.uideveloper@gmail.com>
2020-12-23 14:41:50 +0530

    adding redux

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,6 +1,6 @@
 import { FETCH_PRODUCTS } from "../types";
 
-export const fetchProducts = () => async(dispatch) => {
+export const fetchProducts = () => async (dispatch) => {
     const res = await fetch("/api/products");
     const data = await res.json();
     console.log(data);
@@ -8,4 +8,4 @@ export const fetchProducts = () => async(dispatch) => {
         type: FETCH_PRODUCTS,
         payload: res.data,
     });
-}
+};
