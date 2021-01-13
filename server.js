27b30ad642a673dd21bbce400793889be6a2b54d
unknown <karthi.uideveloper@gmail.com>
2021-01-13 10:46:52 +0530

    Login authentication

--- server.js
+++ server.js
@@ -3,7 +3,20 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const shortid = require("shortid");
 
+const passport = require("passport");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Load input validation
+const validateRegisterInput = require("./backend/validation/register");
+const validateLoginInput = require("./backend/validation/login");
+
+
 const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
 app.use(bodyParser.json());
 
 /* To render static file inside build folder */
@@ -11,13 +24,18 @@ app.use("/", express.static(__dirname + "/dist"));
 /*instead of root.index.html, to render automatically from root address of our site */
 app.get("/", (req, res) => res.sendFile(__dirmane + "/dist/index.html"));
 
+// Connect to MongoDB
 mongoose.connect(
     process.env.MONGODB_URL || "mongodb://localhost/kadal2kitchen-db",
     {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-});
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      secretOrKey: "secret"
+    }
+)
+.then(() => console.log("MongoDB successfully connected")) //remove this afterwards
+.catch(err => console.log(err)); //remove this afterwards
 
 const Product = mongoose.model(
     "products",
@@ -46,6 +64,7 @@ app.delete("/api/products/:id", async (req, res) => {
     const deletedProduct = await Product.findByIdAndDelete(req.params.id);
     res.send(deletedProduct);
 })
+
 
 /* First parameter is the name of the collection in the database,
 second one is schema. */
@@ -77,28 +96,147 @@ const Order = mongoose.model(
 );
 
 app.post("/api/orders", async (req, res) => {
-    if (
-      !req.body.name ||
-      !req.body.email ||
-      !req.body.address ||
-      !req.body.total ||
-      !req.body.cartItems
-    ) {
-      return res.send({ message: "Data is required." });
-    }
-    const order = await Order(req.body).save();
-    res.send(order);
+  if (
+    !req.body.name ||
+    !req.body.email ||
+    !req.body.address ||
+    !req.body.total ||
+    !req.body.cartItems
+  ) {
+    return res.send({ message: "Data is required." });
+  }
+  const order = await Order(req.body).save();
+  res.send(order);
 });
 app.get("/api/orders", async (req, res) => {
-    const orders = await Order.find({});
-    res.send(orders);
+  const orders = await Order.find({});
+  res.send(orders);
 });
 app.delete("/api/orders/:id", async (req, res) => {
-    const order = await Order.findByIdAndDelete(req.params.id);
-    res.send(order);
+  const order = await Order.findByIdAndDelete(req.params.id);
+  res.send(order);
+});
+//------------------------------
+
+const User = mongoose.model(
+  "users",
+  new mongoose.Schema(
+    {
+      name: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: true
+      },
+      password: {
+        type: String,
+        required: true
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  )
+);
+
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+router.post("/register", (req, res) => {
+  // Form validation
+  const { errors, isValid } = validateRegisterInput(req.body);
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+      // Hash password before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("serve at  http://localhost:5000"));
+// @route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
+router.post("/login", (req, res) => {
+  // Form validation
+const { errors, isValid } = validateLoginInput(req.body);
+// Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+const email = req.body.email;
+  const password = req.body.password;
+// Find user by email
+  User.findOne({ email }).then(user => {
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+// Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User matched
+        // Create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+// Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 31556926 // 1 year in seconds
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
+  });
+});
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./backend/config/passport")(passport);
+
+// Routes
+app.use("/api/users", router);
+
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
 
--- src/App.js
+++ src/App.js
@@ -9,9 +9,38 @@ import Fish4 from './images/cut-fishes.jpg';
 
 import store from "./store";
 import { Provider } from "react-redux";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import AdminScreen from "./screens/AdminScreen";
+import Login from "./components/auth/Login";
+
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authentication/authActions";
+
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+    // Set auth token header auth
+    const token = localStorage.jwtToken;
+    setAuthToken(token);
+    // Decode token and get user info and exp
+    const decoded = jwt_decode(token);
+    // Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded));
+  
+    // Check for expired token
+    const currentTime = Date.now() / 1000; // to get in milliseconds
+    if (decoded.exp < currentTime) {
+      // Logout user
+      store.dispatch(logoutUser());
+      // Redirect to login
+      window.location.href = "./login";
+    }
+}
+  
 
 //feature 1
 class App extends React.Component {
@@ -26,12 +55,17 @@ class App extends React.Component {
                            <Link to="/">
                               <img src={FishIcon} alt="Kadal to Kitchen logo" />
                            </Link>                       
-                           <Link to="/admin" className="text-light">Admin</Link>
+                           <Link to="/admin" className="text-light">Admin</Link>                      
+                           <Link to="/login" className="text-light">Login</Link>
                         </div>
                      </header>
                      <main>
                         <Route path="/admin" component={AdminScreen} />
                         <Route path="/" component={HomeScreen} exact />
+                            <Route exact path="/login" component={Login} />
+                            <Switch>
+                                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                            </Switch>
                      </main>
                      
                      <footer className="d-flex justify-content-center align-items-center">
